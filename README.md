cbdefcc227b4049d43aaefcce79d5884f468e44f
Daniel <nieltansah@gmail.com>
2018-09-25 21:36:30 +0700

    Initial commit
    Create TypeScript server project:
    
    - Use husky + lint-staged as commit sanitizer
    - Use own mechanism with chokidar for hot-reloading
    - Use tslint + prettier plugin for linting

---
+++ README.md
@@ -0,0 +1,39 @@
+# travel-ka
+
+A site for managing trains and their schedules.
+
+By:
+
+- 13515029 Finiko Kasula Novenda ([FNicon](https://github.com/FNicon))
+- 13515046 Lathifah Nurrahmah ([tifahnurr](https://github.com/tifahnurr))
+- 13515071 Daniel Pintara ([nieltg](https://github.com/nieltg))
+- 13515089 Vincent Hendryanto Halim ([vincenthend](https://github.com/vincenthend))
+
+## Getting Started
+
+### Requirements
+
+To get started with this project, you need these:
+
+- [Node.js](https://nodejs.org/en/download) as the runtime.
+- [Yarn](https://yarnpkg.com/en/docs/install) package manager.
+
+### Development
+
+To prepare this project:
+
+```
+$ yarn
+```
+
+Development server can be started by:
+
+```
+$ yarn start:dev
+```
+
+You can edit the code in `server` directory and the changes will be reflected in the next request.
+
+# License
+
+[MIT](LICENSE)
---
+++ cli.ts
@@ -0,0 +1,63 @@
+import { watch } from "chokidar"
+import { normalize } from "path"
+
+import * as Koa from "koa"
+import * as yargs from "yargs"
+
+const middlewareModulePath = "./server"
+
+// Preload
+require(middlewareModulePath)
+
+// Argument parser
+
+interface AppArgs extends yargs.Arguments {
+  watch: boolean
+  port: number
+}
+
+const argv = yargs
+  .option("watch", {
+    description: "Hot-reload if managed source-code is changed",
+    type: "boolean"
+  })
+  .option("port", {
+    alias: "p",
+    default: process.env["PORT"] || 8000,
+    defaultDescription: "(env.PORT)",
+    type: "number"
+  })
+  .help()
+  .strict().argv as AppArgs
+
+// Server app.
+
+const app = new Koa()
+
+app.use((...arg) => {
+  return require(middlewareModulePath).default(...arg)
+})
+
+app.listen(argv.port)
+console.error(`Listening on ${argv.port}.`)
+
+// Hot-reloading
+// https://codeburst.io/dont-use-nodemon-there-are-better-ways-fc016b50b45e
+
+if (argv.watch) {
+  const watcher = watch(middlewareModulePath)
+  console.error(`Watching managed source-code on '${middlewareModulePath}'.`)
+
+  const cacheIdPattern = new RegExp(
+    normalize(`/${middlewareModulePath}/`).replace(/[\/\\]/g, "[\\/\\\\]")
+  )
+
+  watcher.on("ready", () => {
+    watcher.on("all", () => {
+      console.error("Changes is detected. Clearing caches.")
+      Object.keys(require.cache).forEach(path => {
+        if (cacheIdPattern.test(path)) delete require.cache[path]
+      })
+    })
+  })
+}

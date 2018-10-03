2558e02326a5801bf5fc55f017d19c0cd570cc17
Daniel <nieltansah@gmail.com>
2018-10-03 19:32:14 +0700

    Remove model/ from source code
    Prev. design of model/ is too excessive. We don't want a new ORM, eh?

--- src/model/train.ts
+++
@@ -1,9 +0,0 @@
-import { BaseModelData, model } from "./base"
-
-interface TrainData extends BaseModelData {
-  name: string
-  manufacturedAt: Date
-  endedAt: Date
-}
-
-export class Train extends model<TrainData>("train") {}

5fdef9b481aba631c42b5c78f425d72e17fb50b9
Daniel <nieltansah@gmail.com>
2018-10-03 11:09:30 +0700

    Revert "Use another approach for knex config. placement"
    This reverts commit 72487185ca373c8aabcc6a0ae7b1368dced19f3e.
    
    Migration files, etc shouldn't be placed inside src/ because they aren't
    part of the application itself.

--- src/config/knex.ts
+++ src/config/knex.ts
@@ -1,22 +1,24 @@
 import { Config } from "knex"
 
-const commonConfig: Config = {
-  client: "postgresql",
-  connection: process.env.DATABASE_URL || {
-    database: "travel_ka",
-    user: "travel_ka",
-    password: "travel_ka"
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: "knex_migrations"
+export function build() {
+  const commonConfig: Config = {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL || {
+      database: "travel_ka",
+      user: "travel_ka",
+      password: "travel_ka"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
   }
-}
 
-export = {
-  development: commonConfig,
-  production: commonConfig
+  return {
+    development: commonConfig,
+    production: commonConfig
+  }
 }

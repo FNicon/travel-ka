4faa302da2e3544844b1babc29f2f32ce9d164e0
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-10-03 20:07:22 +0700

    Add queries pseudocode

--- src/controller/train_schedule.ts
+++ src/controller/train_schedule.ts
@@ -9,22 +9,30 @@ export function apply(router: KoaRouter) {
 
   router.get("schedule_insert", "/schedule/insert", async ctx => {
     await ctx.render("hello", {
+      /*INSERT INTO public.schedule(id, "trainId", source, destination, "departedAt", "arrivedAt")
+        VALUES (?, ?, ?, ?, ?, ?);*/
       youAreUsingPug: true
     })
   })
   router.get("schedule_update", "/schedule/update", async ctx => {
     await ctx.render("hello", {
+      /*UPDATE public.schedule SET id=?, "trainId"=?, source=?, destination=?, "departedAt"=?, "arrivedAt"=?
+	      WHERE <condition>;*/
       youAreUsingPug: true
     })
   })
   router.get("schedule_delete", "/schedule/delete", async ctx => {
     await ctx.render("hello", {
+      /*DELETE FROM public.schedule
+	      WHERE <condition>;*/
       youAreUsingPug: true
     })
   })
 
   router.get("schedule_select", "/schedule/select", async ctx => {
     await ctx.render("hello", {
+      /*SELECT id, "trainId", source, destination, "departedAt", "arrivedAt"
+	      FROM public.schedule;*/
       youAreUsingPug: true
     })
   })
@@ -51,28 +59,38 @@ export function apply(router: KoaRouter) {
 
   router.get("schedule_before", "/schedule/before", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xe < ys; */
+      /*SELECT * FROM public.schedule WHERE "arrivedAt"x < "departedAt"y;*/
       youAreUsingPug: true
     })
   })
   router.get("schedule_after", "/schedule/after", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xs > ye; */
+      /*SELECT * FROM public.schedule WHERE "departedAt"x > "arrivedAt"y;*/
       youAreUsingPug: true
     })
   })
 
   router.get("schedule_meet", "/schedule/meet", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xe == ys;*/
+      /*SELECT * FROM public.schedule WHERE "arrivedAt"x == "departedAt"y;*/
       youAreUsingPug: true
     })
   })
   router.get("schedule_meet_inverse", "/schedule/meet_inverse", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE ys == xe;*/
+      /*SELECT * FROM public.schedule WHERE "departedAt"x == "arrivedAt"y;*/
       youAreUsingPug: true
     })
   })
 
   router.get("schedule_overlap", "/schedule/overlap", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE (xs,xe) OVERLAPS (ys,ye); */
+      /*SELECT * FROM public.schedule WHERE ("departedAt"x,"arrivedAt"x) OVERLAPS ("departedAt"y,"arrivedAt"y);*/
       youAreUsingPug: true
     })
   })
@@ -81,6 +99,8 @@ export function apply(router: KoaRouter) {
     "/schedule/overlap_inverse",
     async ctx => {
       await ctx.render("hello", {
+        /*SELECT * FROM public.schedule WHERE (ys,ye) OVERLAPS (xs,xe); */
+        /*SELECT * FROM public.schedule WHERE ("departedAt"y,"arrivedAt"y) OVERLAPS ("departedAt"x,"arrivedAt"x);*/
         youAreUsingPug: true
       })
     }
@@ -88,17 +108,24 @@ export function apply(router: KoaRouter) {
 
   router.get("schedule_start", "/schedule/start", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xs == ys AND xe < ye;*/
+      /*SELECT * FROM public.schedule WHERE "departedAt"x == "departedAt"y AND "arrivedAt"x < "arrivedAt"y;*/
       youAreUsingPug: true
     })
   })
   router.get("schedule_start_inverse", "/schedule/start_inverse", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xs == ys AND xe > ye;*/
+      /*SELECT * FROM public.schedule WHERE "departedAt"x == "departedAt"y AND "arrivedAt"x > "arrivedAt"y;*/
       youAreUsingPug: true
     })
   })
 
   router.get("schedule_during", "/schedule/during", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE (xs BETWEEN (ys, ye)) AND (xe BETWEEN (ys, ye));*/
+      /*SELECT * FROM public.schedule WHERE ("departedAt"y < "departedAt"x AND "departedAt"x < "arrivedAt"y) 
+                                        AND ("departedAt"y < "arrivedAt"x AND "arrivedAt"x < "arrivedAt"y);*/
       youAreUsingPug: true
     })
   })
@@ -107,6 +134,9 @@ export function apply(router: KoaRouter) {
     "/schedule/during_inverse",
     async ctx => {
       await ctx.render("hello", {
+        /*SELECT * FROM public.schedule WHERE (ys BETWEEN (xs, xe)) AND (ye BETWEEN (xs, xe));*/
+        /*SELECT * FROM public.schedule WHERE ("departedAt"x < "departedAt"y AND "departedAt"y < "arrivedAt"x) 
+                                          AND ("departedAt"x < "arrivedAt"y AND "arrivedAt"y < "arrivedAt"x);*/
         youAreUsingPug: true
       })
     }
@@ -114,6 +144,8 @@ export function apply(router: KoaRouter) {
 
   router.get("schedule_finish", "/schedule/finish", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xe == ye AND xs > ys;*/
+      /*SELECT * FROM public.schedule WHERE "arrivedAt"x == "arrivedAt"y AND "departedAt"x > "departedAt"y;*/
       youAreUsingPug: true
     })
   })
@@ -122,6 +154,8 @@ export function apply(router: KoaRouter) {
     "/schedule/finish_inverse",
     async ctx => {
       await ctx.render("hello", {
+        /*SELECT * FROM public.schedule WHERE xe == ye AND xs < ys;*/
+        /*SELECT * FROM public.schedule WHERE "arrivedAt"x == "arrivedAt"y AND "departedAt"x < "departedAt"y;*/
         youAreUsingPug: true
       })
     }
@@ -129,6 +163,8 @@ export function apply(router: KoaRouter) {
 
   router.get("schedule_equal", "/schedule/equal", async ctx => {
     await ctx.render("hello", {
+      /*SELECT * FROM public.schedule WHERE xs == ys AND xe == ye;*/
+      /*SELECT * FROM public.schedule WHERE "departedAt"x == "departedAt"y AND "arrivedAt"x == "arrivedAt"y;*/
       youAreUsingPug: true
     })
   })

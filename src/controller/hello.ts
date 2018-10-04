1edc12244c2763be4cf034cf43ea5b7ace99c89a
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2018-10-05 00:20:08 +0700

    Added front page

--- src/controller/hello.ts
+++ src/controller/hello.ts
@@ -2,8 +2,6 @@ import * as KoaRouter from "koa-router"
 
 export function apply(router: KoaRouter) {
   router.get("hello", "/hello", async ctx => {
-    await ctx.render("hello", {
-      youAreUsingPug: true
-    })
+    await ctx.render("hello")
   })
 }

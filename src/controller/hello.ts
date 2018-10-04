56dd45315c7746002afadeda0b218a55239816ba
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2018-10-05 01:18:11 +0700

    Added UI for list

--- src/controller/hello.ts
+++ src/controller/hello.ts
@@ -1,7 +1,7 @@
 import * as KoaRouter from "koa-router"
 
 export function apply(router: KoaRouter) {
-  router.get("hello", "/hello", async ctx => {
+  router.get("hello", "/", async ctx => {
     await ctx.render("hello")
   })
 }
